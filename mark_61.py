3da0b612e4fc57bb7e66a44623e01395a7b622b7
Sunny Gawande <66326229+SUNNY-GAWANDE@users.noreply.github.com>
2024-05-30 18:25:38 +0100

    Update mark_61.py

--- mark_61.py
+++ mark_61.py
@@ -28,19 +28,19 @@ pd.set_option('display.max_columns', 500)
 # In[3]:
 
 
-df_month_location = pd.read_csv('https://github.com/SUNNY-GAWANDE/Dissertation/blob/main/df_month_location1.csv', encoding="latin")
+df_month_location = pd.read_csv('https://github.com/SUNNY-GAWANDE/Dissertation/blob/main/df_month_location1.csv', sep=',')
 
 
 # In[4]:
 
 
-df_season_location = pd.read_csv('https://github.com/SUNNY-GAWANDE/Dissertation/blob/main/df_season_location1.csv', encoding="latin")
+df_season_location = pd.read_csv('https://github.com/SUNNY-GAWANDE/Dissertation/blob/main/df_season_location1.csv', sep=',')
 
 
 # In[5]:
 
 
-df_year_location = pd.read_csv('https://github.com/SUNNY-GAWANDE/Dissertation/blob/main/df_year_location1.csv', encoding="latin")
+df_year_location = pd.read_csv('https://github.com/SUNNY-GAWANDE/Dissertation/blob/main/df_year_location1.csv',sep=',')
 
 
 # In[6]:
